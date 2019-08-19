a3441cb3c869ffc3dd03edca9692a5e0b66efbfd
David Forer <david.j.forer@gmail.com>
2019-08-19 00:34:25 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -16,7 +16,15 @@ class Application
       resp.write handle_search(search_term)
       
     elsif req.path.match(/cart/)
-    if @@cart.length > 0
+    
+    if @@cart.empty?
+      resp.write "Your cart is empty"
+      
+    else 
+      @@cart.each do |item|
+        resp.write "#{item}\n"
+    end
+  end
     
     else
       resp.write "Path Not Found"
@@ -32,4 +40,6 @@ class Application
       return "Couldn't find #{search_term}"
     end
   end
+  
+  
 end
