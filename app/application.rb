e914488f62db64b2657a7c08c2d0f4dac750c9ef
David Forer <david.j.forer@gmail.com>
2019-08-19 00:38:06 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -41,5 +41,12 @@ class Application
     end
   end
   
-  
+  def handle_add(add_item)
+    if @@items.include?(add_item)
+      @@cart << add_item 
+      return "added #{add_item}"
+    else
+      return "We don't have that item"
+    end
+  end 
 end
